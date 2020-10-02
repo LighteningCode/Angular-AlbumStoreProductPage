58e0e2ad7f4540faf3e11de12551a1696a9bde7c
LigtheningCode <elvisagbesi@gmail.com>
2020-10-02 19:08:05 +0000

    Creating a service to get album data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,11 +7,9 @@ export class ProductService {
   private _albumUrl = '../assets/album.json'
   constructor(private _http: Http) { }
 
-
   getAlbum(id: number) {
     return this._http.get(this._albumUrl)
-        .map((response) => {
-          return response.json();
-        })
+        .map(response => response.json())
   }
+
 }
